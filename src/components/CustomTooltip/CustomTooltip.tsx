f70f4efe01bea5cbabb67a74e8687bfef45afa2f
Lucas Moreira <lucasmsea@outlook.com>
2024-08-25 16:39:48 -0300

    chore: remove `console.log`

--- src/components/CustomTooltip/CustomTooltip.tsx
+++ src/components/CustomTooltip/CustomTooltip.tsx
@@ -4,9 +4,6 @@ import { TooltipProps } from "recharts";
 
 export const CustomTooltip = ({ payload }: TooltipProps<number, string>) => {
   if (!payload || payload.length === 0) return null;
-  console.log({
-    payload,
-  });
 
   return (
     <div className="bg-white p-2 rounded">
